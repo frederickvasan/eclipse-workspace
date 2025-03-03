5f521632ff60831161fa46be04962b9381c700fa
frederickvasan <fnoble20@gmail.com>
2025-03-03 17:47:36 +0530

    mar 3rd2025

---
+++ cucumbertest/src/test/java/org/cucumber/Test.java
@@ -0,0 +1,11 @@
+package org.cucumber;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+	
+	WebDriver driver = new ChromeDriver();
+	
+
+}
